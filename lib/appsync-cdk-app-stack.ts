fa2f01322e82b11a80480c595682cb1d62ac6f50
Erik Hanchett <ehhanche@amazon.com>
2023-10-13 11:09:59 -0700

    First Commit

--- lib/appsync-cdk-app-stack.ts
+++ lib/appsync-cdk-app-stack.ts
@@ -1,16 +1,111 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+import {
+  AmplifyGraphqlApi,
+  AmplifyGraphqlDefinition,
+} from "@aws-amplify/graphql-api-construct";
+import * as path from "path";
+import * as awsCognito from "aws-cdk-lib/aws-cognito";
+import {
+  IdentityPool,
+  UserPoolAuthenticationProvider,
+} from "@aws-cdk/aws-cognito-identitypool-alpha";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class AppsyncCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda
+    const bedrockLambda = new lambda.Function(this, "bedrockLambda", {
+      functionName: "MyBedrockLambda",
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "handlers/bedrocklambda")
+      ),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_18_X,
+      timeout: cdk.Duration.seconds(300),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    bedrockLambda.grantPrincipal.addToPrincipalPolicy(
+      new PolicyStatement({
+        resources: [
+          "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2",
+        ],
+        actions: ["bedrock:InvokeModel"],
+      })
+    );
+
+    // UserPool
+    const userPool = new awsCognito.UserPool(this, "MyUserPool", {
+      userPoolName: "MyUserPool",
+      selfSignUpEnabled: true,
+      autoVerify: {
+        email: true,
+      },
+      accountRecovery: awsCognito.AccountRecovery.EMAIL_ONLY,
+      userVerification: {
+        emailStyle: awsCognito.VerificationEmailStyle.CODE,
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+    });
+
+    // User Pool Client
+    const userPoolClient = new awsCognito.UserPoolClient(
+      this,
+      "MyUserpoolClient",
+      { userPool }
+    );
+
+    // Identity Pool
+    const identityPool = new IdentityPool(this, "MyIdentityPool", {
+      identityPoolName: "MyIdentityPool",
+      allowUnauthenticatedIdentities: true,
+      authenticationProviders: {
+        userPools: [
+          new UserPoolAuthenticationProvider({
+            userPool: userPool,
+            userPoolClient: userPoolClient,
+          }),
+        ],
+      },
+    });
+
+    // AppSync
+    new AmplifyGraphqlApi(this, "MyNewAPI", {
+      apiName: "MyNewAPI",
+      definition: AmplifyGraphqlDefinition.fromFiles(
+        path.join(__dirname, "schema.graphql")
+      ),
+
+      authorizationModes: {
+        defaultAuthorizationMode: "AMAZON_COGNITO_USER_POOLS",
+        apiKeyConfig: {
+          expires: cdk.Duration.days(30),
+        },
+        userPoolConfig: {
+          userPool,
+        },
+      },
+      functionNameMap: { noteSummary: bedrockLambda },
+    });
+
+    // outputs
+    new cdk.CfnOutput(this, "UserPoolId", {
+      value: userPool.userPoolId,
+    });
+    new cdk.CfnOutput(this, "UserPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+    new cdk.CfnOutput(this, "IdentityPoolId", {
+      value: identityPool.identityPoolId,
+    });
   }
 }
---
+++ lib/handlers/bedrocklambda/index.ts
@@ -0,0 +1,35 @@
+import { AppSyncResolverEvent } from "aws-lambda";
+import { BedrockRuntime } from "@aws-sdk/client-bedrock-runtime";
+
+type AppSyncEvent = {
+  msg: string;
+};
+
+export const handler = async (event: AppSyncResolverEvent<AppSyncEvent>) => {
+  const prompt = `Take the following string and summarize it into one sentence, make sure to only return the summary only, no other text please. "${event.arguments.msg}"`;
+  const claudePrompt = `\n\nHuman: ${prompt} \n\nAssistant:`;
+  const config = {
+    prompt: claudePrompt,
+    max_tokens_to_sample: 2048,
+    temperature: 0.5,
+    top_k: 250,
+    top_p: 1,
+    stop_sequences: ["\n\nHuman:"],
+  };
+  const bedrock = new BedrockRuntime({ region: "us-east-1" });
+  try {
+    const response = await bedrock.invokeModel({
+      body: JSON.stringify(config),
+      modelId: "anthropic.claude-v2",
+      accept: "application/json",
+      contentType: "application/json",
+    });
+    const value = JSON.parse(response.body.transformToString());
+    console.log(value.completion);
+    console.log(`\nStop reason: ${value.stop_reason}`);
+    return value.completion;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
